8ff049ba1f30845cac9ee3607883fc4edbe455c5
Никита <nikita.slepnev.98@gmail.com>
2022-02-08 22:22:45 +0800

    Добавьте файлы проекта.

---
+++ ASPController/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPController.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Default");
+        }
+
+        [HttpPost]
+        public string Hello(string name)
+        {
+            return $"Hello {name}";
+        }
+    }
+}
