1659cdddfeb1f4a11f6dcda08691ff15dba9ad05
Nikita <nikita.slepnev.98@gmail.com>
2022-02-14 18:34:02 +0800

    Изменение\Добавление товаров

---
+++ ASPController_PC/FormAddOrEditProduct.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace ASPController_PC
+{
+    partial class FormAddOrEditProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.comboBoxPost = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(24, 22);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(453, 26);
+            this.textBoxLastName.TabIndex = 0;
+            this.textBoxLastName.Text = "Наименование товара";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(24, 156);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(453, 26);
+            this.textBoxEmail.TabIndex = 5;
+            this.textBoxEmail.Text = "Стоимость";
+            // 
+            // comboBoxPost
+            // 
+            this.comboBoxPost.FormattingEnabled = true;
+            this.comboBoxPost.Location = new System.Drawing.Point(24, 221);
+            this.comboBoxPost.Name = "comboBoxPost";
+            this.comboBoxPost.Size = new System.Drawing.Size(450, 27);
+            this.comboBoxPost.TabIndex = 4;
+            this.comboBoxPost.Text = "Состояние";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(24, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(453, 56);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonAddPerson_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(453, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "Описание";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 188);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(450, 27);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "Поставщик";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(24, 254);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(450, 27);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.Text = "Склад";
+            // 
+            // FormAddOrEditProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(504, 408);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxPost);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxLastName);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FormAddOrEditProduct";
+            this.Text = "Добавление товара";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.ComboBox comboBoxPost;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ ASPController_PC/FormAddOrEditProduct.cs
@@ -0,0 +1,68 @@
+﻿using CommonData;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASPController_PC
+{
+    public partial class FormAddOrEditProduct: Form
+    {
+        Person person = null;
+        public FormAddOrEditProduct(Person person = null)
+        {
+            InitializeComponent();
+            //dateTimePickerBirth.Value = DateTime.Now;
+            //LoadData();
+            //if(person != null)
+            //{
+            //    this.person = person;
+            //    textBoxLastName.Text = person.LastName;
+            //    textBoxName.Text = person.Name;
+            //    textBoxMiddleName.Text = person.MiddleName;
+            //    dateTimePickerBirth.Value = person.DataBirth == null ? DateTime.Now : person.DataBirth.Value;
+            //    textBoxEmail.Text = person.Email;
+            //    textBoxNumber.Text = person.PhoneNumber;
+                
+            //    this.Text = "Изменение физ.лица";
+            //    button1.Text = "Сохранить изменения";
+            //}
+            //else
+            //{
+            //    this.person = new Person();
+            //}
+        }
+
+        private async void LoadData()
+        {
+            var data = await Post.GetPosts();
+            comboBoxPost.DataSource = new ObservableCollection<Post>(data).ToBindingList();
+            if (!string.IsNullOrEmpty(Post.GetException()))
+                MessageBox.Show(Post.GetException());
+            else
+            {
+                if (person.Post != null)
+                    comboBoxPost.SelectedItem = comboBoxPost.Items.Cast<Post>().First(p => p.Id == person.Post.Id);
+            }
+        }
+
+        private void buttonAddPerson_Click(object sender, EventArgs e)
+        {
+            //person.LastName = textBoxLastName.Text;
+            //person.Name = textBoxName.Text;
+            //person.MiddleName = textBoxMiddleName.Text;
+            //person.DataBirth = ((DateTime?)dateTimePickerBirth.Value.Date) == ((DateTime?)DateTime.Now.Date) ? null : (DateTime?)dateTimePickerBirth.Value.Date;
+            //person.Email = textBoxEmail.Text;
+            //person.PhoneNumber = textBoxNumber.Text;
+            //person.Post = comboBoxPost.SelectedIndex >= 0 ? (Post)comboBoxPost.SelectedItem : null;
+            //Person.AddOrEditPerson(person);
+        }
+    }
+}
